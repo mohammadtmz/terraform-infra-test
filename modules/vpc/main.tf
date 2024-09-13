130fcb77ce6b999a9767ba7a6a86968b955092df
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-13 16:17:26 -0400

    az updated

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public"{
     vpc_id = aws_vpc.main.id
-    availability_zone = var.az_id
+    availability_zone = var.az
     cidr_block = var.public_subnet_cidr
     map_public_ip_on_launch = true
     tags = {
@@ -17,7 +17,7 @@ resource "aws_subnet" "public"{
 
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
-    availability_zone = var.az_id
+    availability_zone = var.az
     cidr_block = var.private_subnet_cidr
     tags = {
         Name = "${var.vpc_name}-private"
