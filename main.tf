c154981b4e9d28a9b6148c206bcc4c36ae8f2b37
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-13 18:53:33 -0400

    destroying the instance and remote backend s3

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
 provider "aws" {
     region = "us-east-1"
 }
-
-terraform {
-    backend "s3" {
-        bucket          = "my-terraform-state-bucket-akbar"
-        key             = "akbar/"
-        region          = "us-east-1"
-        dynamodb_table  = "terraform-state-lock"
-        encrypt         = true
-    }
-}
-
-resource "aws_instance" "example" {
-    ami = "ami-0182f373e66f89c85"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = "TerraformExample"
-    }
-}
