b534a26b97f12ef65f250defa1f3ae93d2bfcb32
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-12 22:57:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami = "ami-0182f373e66f89c85"
-  instance_type "t2.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "TerraformExampleInstance"
