1ca57531d61239fde44318a3c8956de2770c5d01
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-13 08:58:57 -0400

    update

--- main.tf
+++ main.tf
@@ -2,15 +2,3 @@ provider "aws" {
   region = "us-east-1"
 }
 
-#resource "aws_instance" "example" {
-#  ami = "ami-0182f373e66f89c85"
-#  instance_type = "t2.micro"
-#
-#  tags = {
-#    Name = "TerraformExampleInstance"
-#  }
-#}
-
-#output "instance_id" {
-#  value = aws_instance.example.id
-#}
