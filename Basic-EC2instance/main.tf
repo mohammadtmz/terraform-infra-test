c30403674e3dcebb525fecfe174f3fa447fcf624
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-13 08:44:56 -0400

    starting to test terrafrom

---
+++ Basic-EC2instance/main.tf
@@ -0,0 +1,18 @@
+# AWS provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a new EC2 instance 
+resource "aws_instance" "example" {
+  ami = "ami-0182f373e66f89c85" # Amazon Linux AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformExampleInstance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
