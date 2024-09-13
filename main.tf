ad36db8c812451c80b2798f9190dc2ce1c60ddd9
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-13 16:13:41 -0400

    az added to the module

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 
 module "vpc" {
     source = "./modules/vpc"
+    az = "us-east-1a"
     vpc_cidr = "10.0.0.0/16"
     public_subnet_cidr = "10.0.1.0/24"
     private_subnet_cidr = "10.0.2.0/24"
