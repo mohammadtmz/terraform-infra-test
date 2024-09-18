df7afb902ea9e6e1e0d7db1b4e4926d2b9838cd8
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-17 22:14:06 -0400

    changine directory

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
 provider "aws" {
     region = "us-east-1"
 }
-
-resource "aws_iam_user" "example" {
-    for_each = { for username, user in var.users : username => user if user.create_user }
-
-    name = each.key
-}
-
-resource "aws_iam_user_policy_attachment" "example" {
-    for_each    = aws_iam_user.example
-    user        = each.key
-    policy_arn  = "arn:aws:iam::aws:policy/${var.users[each.key].policy}"
-}
