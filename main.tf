ba9e17e2ae26feb8536a31019991df59f8510a5b
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-19 23:06:40 -0400

    auto scaling group

--- main.tf
+++ main.tf
@@ -1,3 +1,75 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_launch_template" "asg_template" {
+    name_prefix   = "example-asg-launch-template-"
+    image_id      = var.ami_id
+    instance_type = var.instance_type
+
+    vpc_security_group_ids = var.security_group_ids
+}
+
+resource "aws_autoscaling_group" "example_asg" {
+    name             = "example-asg"
+    desired_capacity = var.min_size
+    min_size         = var.min_size
+    max_size         = var.max_size
+
+    launch_template {
+        id      = aws_launch_template.asg_template.id
+        version = "$Latest"
+    }
+
+    vpc_zone_identifier = var.subnet_ids
+
+    tag {
+        Key                 = "Name"
+        value               = "example-asg-instance"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_autoscaling_policy" "scale_up" {
+    name                    = "scale-up"
+    scaling_adjustment      = 1
+    adjustment_type         = "ChangeInCapacity"
+    autoscaling_group_name  = aws_autoscaling_group.example_asg.name
+}
+
+resource "aws_autoscaling_policy" "scale_down" {
+    name                    = "scale-down"
+    scaling_adjustment      = -1
+    adjustment_type         = "ChangeInCapacity"
+    autoscaling_group_name  = aws_autoscaling_group.example_asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+    alarm_name          = "cpu-high"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_period   = 2
+    metric_name         = "CPUUtilization"
+    namespace           = "AWS/EC2"
+    period              = 120
+    statistic           = "Average"
+    threshold           = 70
+    alarm_actions       = [aws_autoscaling_policy.scale_up.arn]
+    dimensions = {
+        AutoScalingGroupName = aws_autoscaling_group.example_asg.name
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_low" {
+    name                = "cpu-low"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_period   = 2
+    metric_name         = "CPUUtilization"
+    namespace           = "AWS/EC2"
+    period              = 120
+    statistic           = "Average"
+    threshold           = 30
+    alarm_actions       = [aws_autoscaling_policy.scale_down.arn]
+    dimensions = {
+        AutoScalingGroupName = aws_autoscaling_group.example_asg.name
+    }
+}
