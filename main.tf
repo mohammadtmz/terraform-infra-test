0acdd22d572a70b154c7e41cc9d332e23d911dac
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-20 15:43:13 -0400

    adding aws-s3-bucket-acl resource

--- main.tf
+++ main.tf
@@ -13,11 +13,15 @@ resource "random_string" "bucket_id" {
 
 resource "aws_s3_bucket" "example" {
     bucket = "${local.environment}-${random_string.bucket_id.result}-bucket-example"
-    acl     = "private"
     tags = {
         Name = "${local.environment}-bucket-example"
         Environment = local.environment
     }
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+    bucket = aws_s3_bucket.example.bucket_id
+    acl = "private"
 }
 
 output "current_workspace" {
