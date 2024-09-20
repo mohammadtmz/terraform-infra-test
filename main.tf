a1cd5abb90e74d4c0bbd30aade87ee585b0232c2
MohammadTmz <58887856+mohammadtmz@users.noreply.github.com>
2024-09-20 15:34:29 -0400

    multi environment deployment

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+locals {
+    environment = var.environment != "" ? var.environment : terraform.workspace
+}
+
+resource "random_string" "bucket_id" {
+    length = 9
+    special = false
+}
+
+resource "aws_s3_bucket" "example" {
+    bucket = "${local.environment}-${random_string.bucket_id.result}-bucket-example"
+    acl     = "private"
+    tags = {
+        Name = "${local.environment}-bucket-example"
+        Environment = local.environment
+    }
+}
+
+output "current_workspace" {
+    value = terraform.workspace
+}
