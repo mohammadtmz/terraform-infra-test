7c3abcf46896279250042b6d346afda12589dcad
mohammadtmz <mohammadhassantamizi@MohammassansMBP.hitronhub.home>
2024-09-22 17:25:44 -0400

    destroy

--- main.tf
+++ main.tf
@@ -2,26 +2,26 @@ provider "aws" {
     region = "us-east-1"
 }
 
-locals {
-    environment = var.environment != "" ? var.environment : terraform.workspace
-}
+#locals {
+#    environment = var.environment != "" ? var.environment : terraform.workspace
+#}
 
-resource "random_string" "bucket_id" {
-    length = 8
-    special = false
-    upper = false
-    lower = true
-    numeric = true
-}
+#resource "random_string" "bucket_id" {
+#    length = 8
+#    special = false
+#    upper = false
+#    lower = true
+#    numeric = true
+#}
 
-resource "aws_s3_bucket" "example" {
-    bucket = "${local.environment}-${random_string.bucket_id.result}-bucket-example"
-    tags = {
-        Name = "${local.environment}-bucket-example"
-        Environment = local.environment
-    }
-}
+#resource "aws_s3_bucket" "example" {
+#    bucket = "${local.environment}-${random_string.bucket_id.result}-bucket-example"
+#    tags = {
+#        Name = "${local.environment}-bucket-example"
+#        Environment = local.environment
+#    }
+#}
 
-output "current_workspace" {
-    value = terraform.workspace
-}
+#output "current_workspace" {
+#    value = terraform.workspace
+#}
